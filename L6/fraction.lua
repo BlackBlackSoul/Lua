2528c572ec904c218d474316b2e6bd8a8dae16f7
Tooster <max@polarczyk.pl>
2018-04-11 18:20:22 +0200

    Add fractions

--- L6/fraction.lua
+++ L6/fraction.lua
@@ -1,28 +1,113 @@
 
-local fractions = {mt={}}
+local Frac = {mt={}}
 
-require("math")
+local abs = math.abs
+local modf = math.modf
+local floor = math.floor 
+local precision = 30
+local FMT = {}
+FMT.__call = function (_, ...)
+  return Frac.new(...)
+end
+setmetatable(Frac, FMT)
 
-function fractions.new(numerator, denominator) 
-    if denominator == 0 then error "denominator cannot be 0" end
-    local gcd = fractions.gcd(numerator,denominator)
-    local frac = {num = numerator/gcd,den = denominator/gcd}
-    setmetatable(frac, fractions.mt)
-    return frac
+function Frac.new(num, den) 
+    if den == 0 then error "denominator cannot be 0" end
+    local frac = {num = num,den = den}
+    setmetatable(frac, Frac.mt)
+    return Frac.normalize(frac)
 end
 
-function fractions.add(f1, f2)
-  local _num = f1.num*f2.den + f2.num*f1.num
-  local _den = f1.den*f2.den
-  local gcd = fractions.gcd(_num, _den)
-  return fractions.new(_num/gcd, _den/gcd)
+Frac.mt.__unm = function (f1)
+    f1 = Frac.toFrac(f1)
+    f1.num = f1.num*(-1)
+    return f1
 end
 
-fractions.mt.__add = fractions.add
+Frac.mt.__add = function (f1, f2)
+  f1 = Frac.toFrac(f1)
+  f2 = Frac.toFrac(f2)
+  return Frac(f1.num*f2.den + f2.num*f1.num, f1.den*f2.den)
+end
 
-function fractions.gcd(m, n)
+Frac.mt.__sub = function (f1, f2) 
+  f1 = Frac.toFrac(f1)
+  f2 = Frac.toFrac(f2)
+  return f1+(-f2) end
+
+Frac.mt.__mul = function (f1, f2)
+  f1 = Frac.toFrac(f1)
+  f2 = Frac.toFrac(f2)
+  return Frac(f1.num*f2.num, f1.den*f2.den)
+end
+
+Frac.mt.__div = function (f1, f2)
+  f1 = Frac.toFrac(f1)
+  f2 = Frac.toFrac(f2)
+  f2.num, f2.den = f2.den, f2.num
+  return f1*f2
+end
+
+Frac.mt.__tostring = function (f1)  
+  f1 = Frac.toFrac(f1)
+  return (f1.num).."/"..(f1.den) 
+end
+
+Frac.mt.__eq = function(f1, f2)  
+  f1 = Frac.toFrac(f1)
+  f2 = Frac.toFrac(f2)
+  return f1.num == f2.num and f1.den == f2.den end
+
+Frac.mt.__lt = function(f1, f2)  
+  f1 = Frac.toFrac(f1)
+  f2 = Frac.toFrac(f2)
+  return f1.num*f2.den < f2.num*f1.den end
+
+Frac.mt.__le = function(f1, f2)  
+  f1 = Frac.toFrac(f1)
+  f2 = Frac.toFrac(f2)
+  return not f2 < f1 end
+
+Frac.mt.__concat = function(f1, f2)  
+  f1 = Frac.toFrac(f1)
+  f2 = Frac.toFrac(f2)
+  return tostring(f1)..tostring(f2) end
+
+Frac.mt.__pow = function(f, exp)
+  f = Frac.toFrac(f)
+  if type(exp) ~= "number" then error("Exponent must be a number.") end
+  return Frac(f.num^exp, f.den^exp)
+end
+
+function Frac.toFloat(f1)
+  return f1.num/f1.den
+end
+
+function Frac.gcd(m, n)
     while m ~= 0 do m, n = (n % m), m; end
     return n;
 end
 
-return fractions
+function Frac.normalize(frac)
+  local _num = frac.num  local _den = frac.den
+  local sgn = (_num/abs(_num))*(_den/abs(_den))
+  _num = abs(_num) _den = abs(_den)
+  local gcd = Frac.gcd(_num, _den)
+  frac.num = sgn*_num/gcd
+  frac.den = _den/gcd
+  return frac
+end
+
+function Frac.toFrac(d) 
+  if getmetatable(d) == Frac.mt then return d end
+  local sgn = d/abs(d)
+  d = abs(d)
+  local num1; local num2
+  local num1, num2 = modf(d, d+100)
+  local rem = string.sub(tostring(num2), 3)
+  num1 = num1 * (10^(#rem))
+  num2 = tonumber(rem)
+  local den = 1
+  return Frac(sgn*(num1 + num2), den * (10^(#rem)))
+end
+return Frac
--- L6/test.lua
+++ L6/test.lua
@@ -1,8 +1,22 @@
 local fractions = require("L6/fraction")
-require("math")
 
-local f1 = fractions.new(1,2)
-local f2 = fractions.new(2,3)
+local f1 = fractions(1,2)
+local f2 = fractions(2,3)
 
-print(math.modf(5,3))
+local x = getmetatable(f1)
+
 print(f1+f2)
+print(f1-f2)
+print(f1*f2)
+print(f1/f2)
+print(f1^2)
+
+print(f1+10.0)
+print(f1-10.0)
+print(f1*10.0)
+print(f1/10.0)
+print(f1^2)
+
+print(fractions.toFloat(f1))
+
+print(fractions.toFrac(0.123))
