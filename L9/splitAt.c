d98136052b6446ea1e2cfbc36ef6ed20d790f1a1
Tooster <max@polarczyk.pl>
2018-05-23 16:22:49 +0200

    Add L9 splitAt - UNTESTED

--- L9/splitAt.c
+++ L9/splitAt.c
@@ -7,8 +7,18 @@
 
 static int l_splitAt(lua_State *L) {
     if (!lua_istable(L, 1))
-        luaL_error(L, "First argument must be a 1 argument lambda.");
-    return 1;
+        luaL_error(L, "First argument must be a table.");
+    size_t n = lua_rawlen(L, 1);
+    for (int elem = 2, it = 1; elem <= lua_gettop(L); elem++) { // <= ? < ?
+        lua_newtable(L);
+        int tab = lua_gettop(L); // table on stack
+        lua_Integer m = lua_tointeger(L, elem); // number of elements for table
+        for (int i = 1; i <= m && it <= n; ++i, ++it) {
+            lua_geti(L, 1, it);
+            lua_seti(L, tab, i);
+        }
+    }
+    return (int) n;
 }
 
 static const struct luaL_Reg splitAt[] = {
